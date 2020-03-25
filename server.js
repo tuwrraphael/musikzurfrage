6fe058c52af12a15154fc26ac38cdd5851a5d499
Raphael Hauk <raphael.hauk@tis-cca.com>
2020-03-25 18:22:59 +0100

    wss server

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
+const fs = require('fs');
+const minimist = require('minimist');
 const WebSocket = require('ws');
+const https = require('https');
 
-const wss = new WebSocket.Server({ port: 7887 });
+let args = minimist(process.argv.slice(2), {
+  default: {
+    private: null,
+    public: null
+  }
+});
+let opts = {
+  port: 7887
+};
+
+if (args.private && args.public) {
+  console.log("using https");
+  let privateKey = fs.readFileSync(args.private, 'utf8');
+  let certificate = fs.readFileSync(args.public, 'utf8');
+  let httpsServer = https.createServer({ key: privateKey, cert: certificate });
+  httpsServer.listen(opts.port);
+  opts = { server: httpsServer };
+}
+
+const wss = new WebSocket.Server(opts);
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
